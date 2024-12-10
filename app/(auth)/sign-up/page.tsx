28f06f1fe283224eb012fef535ec3503b6680f5f
binva12 <quocdat3b@yahoo.com.vn>
2024-12-10 15:30:29 +0700

    MobileNav & NavLink

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,16 @@
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validation";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType='SIGN_UP'
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignUp;
---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const FindJobs = (props: Props) => {
+  return <div>FindJobs</div>;
+};
+
+export default FindJobs;
