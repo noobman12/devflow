c3cfb3600ca247a574ec1d4da7d1ad25e8e797ca
binva12 <quocdat3b@yahoo.com.vn>
2025-01-28 13:43:56 +0700

    Implement Local Search Bar

--- constants/routes.ts
+++ constants/routes.ts
@@ -5,5 +5,6 @@ const ROUTES = {
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
   COMMUNITY: "/community",
+  ASK_QUESTION: "/ask-question",
 };
 export default ROUTES;
